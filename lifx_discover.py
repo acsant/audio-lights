e6b7bc29d1df11668265adb75ef25bdd0c2c9181
Akash Sant <acsant@uwaterloo.ca>
2018-09-01 05:56:43 -0700

    Added a few comments and code cleanup
    - Made some code cleanup
    - Added few comments
    - Fixed spellings and added meaningful naming

--- lifx_discover.py
+++ lifx_discover.py
@@ -4,28 +4,28 @@ import sys
 
 from lifxlan import LifxLAN
 
-
+# Could you please source this file? Also run a linter on it. Overall, great job!
 def main():
-    num_lights = None
+    number_of_lights = None
     if len(sys.argv) != 2:
         print("\nDiscovery will go much faster if you provide the number of lights on your LAN:")
-        print("  python {} <number of lights on LAN>\n".format(sys.argv[0]))
+        print("python {} <number of lights on LAN>\n".format(sys.argv[0]))
     else:
-        num_lights = int(sys.argv[1])
+        number_of_lights = int(sys.argv[1])
 
-    # instantiate LifxLAN client, num_lights may be None (unknown).
-    # In fact, you don't need to provide LifxLAN with the number of bulbs at all.
-    # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance
+    # instantiate LifexLAN client, number_of_lights may be None (unknown).
+    # In fact, you don't need to provide LifexLAN with the number of bulbs at all.
+    # lifex = LifexLAN() works just as well. Knowing the number of bulbs in advance
     # simply makes initial bulb discovery faster.
-    print("Discovering lights...")
-    lifx = LifxLAN(num_lights)
+    print("Please Wait - Discovering lights...")
+    lifeX = LifxLAN(number_of_lights)
 
     # get devices
-    devices = lifx.get_lights()
+    devices = lifeX.get_lights()
     print("\nFound {} light(s):\n".format(len(devices)))
-    for d in devices:
+    for device in devices:
         try:
-                print(d)
+            print(device)
         except:
             pass
 
